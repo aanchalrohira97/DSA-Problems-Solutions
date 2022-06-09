87f00d0d9127734473d60e6f8ca8630c20283326
Aanchal Rohira <aanchalrohira97@gmail.com>
2022-06-08 21:57:08 -0400

    Time: 25 ms (17.05%), Space: 46.3 MB (20.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -9,23 +9,26 @@ class Solution {
          */
         //plan : 1. compare and negate the frequency from second string
         
-        //implement:
+        //implement: taking HashMap, add all alphabets that exist with their freq
+                //: in the second loop, remove them from the hashmap if frequency reaches 0
             
         if (s.length() != t.length()) return false;
         
-        int[] alphabets = new int[26];
+      
+        Map<Character, Integer> freq = new HashMap<>();
         
         for(int i=0;i<s.length();i++){
-            alphabets[s.charAt(i) - 'a']++;
-            alphabets[t.charAt(i)-'a']--;
+            int value = freq.getOrDefault(s.charAt(i),0);
+            freq.put(s.charAt(i),value+1);
         }
         
-        for(int i=0;i<alphabets.length;i++){
-            if(alphabets[i]!=0){
-                return false;
-            }
+        for(int i=0;i<t.length();i++)
+        {
+            int value = freq.getOrDefault(t.charAt(i),0);
+            freq.put(t.charAt(i),value-1);
+            if(freq.get(t.charAt(i))==0) freq.remove(t.charAt(i));
         }
         
-        return true;
+        return freq.isEmpty();
     }
 }
