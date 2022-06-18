6ff6e0bd8c8b04d265b6a9411313fd8d30614f12
Aanchal Rohira <aanchalrohira97@gmail.com>
2022-06-18 18:11:53 -0400

    Time: 15 ms (5.59%), Space: 42.4 MB (51.98%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -1,54 +1,33 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Node root = new Node();
-        for (String word : wordDict)
-            addWord(word, root);
-        return wordBreak(s, 0, new HashMap<>(), root);
+        Set<String> set = new HashSet<>();
+        for(String word : wordDict)
+            set.add(word);
+        return wordBreak(s, set);
     }
     
-    public void addWord(String word, Node root) {
-        for(int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (!root.map.containsKey(c))
-                root.map.put(c, new Node());
-            root = root.map.get(c);
-        }
-        root.end = true;
-    }
-    
-    public List<String> wordBreak(String s, int index, Map<Integer, List<String>> dp, Node root) {
-        if (index == s.length()) {
-            return Collections.emptyList();
-        }
+public List<String> wordBreak(String s, Set<String> wordDict) {
+    return DFS(s, wordDict, new HashMap<String, LinkedList<String>>());
+}       
+
+// DFS function returns an array including all substrings derived from s.
+List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>>map) {
+    if (map.containsKey(s)) 
+        return map.get(s);
         
-        if (dp.containsKey(index)) {
-            return dp.get(index);
+    LinkedList<String>res = new LinkedList<String>();     
+    if (s.length() == 0) {
+        res.add("");
+        return res;
+    }               
+    for (String word : wordDict) {
+        if (s.startsWith(word)) {
+            List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
+            for (String sub : sublist) 
+                res.add(word + (sub.isEmpty() ? "" : " ") + sub);               
         }
-        
-        Node curr = root;
-        for(int i = index; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!curr.map.containsKey(c))
-                break;
-            if (curr.map.get(c).end) {
-                List<String> out = wordBreak(s, i + 1, dp, root);
-                List<String> result = dp.getOrDefault(index, new ArrayList<>());
-                String sub = s.substring(index, i + 1);
-                if (i + 1 == s.length())
-                    result.add(sub);
-                else
-                    for (String o : out)
-                        result.add(sub + " " + o);
-                dp.put(index, result);
-            }
-            curr = curr.map.get(c);
-        }
-        return dp.getOrDefault(index, new ArrayList<>());
-    }
-    
-    
-    public static class Node {
-        Map<Character, Node> map = new HashMap<>();
-        boolean end = false;
-    }
+    }       
+    map.put(s, res);
+    return res;
+}
 }
