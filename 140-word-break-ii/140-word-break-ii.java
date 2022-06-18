1e74650dd4b8bac32b0d3aabb38ef847971da471
Aanchal Rohira <aanchalrohira97@gmail.com>
2022-06-18 18:48:22 -0400

    Time: 1 ms (98.94%), Space: 42.4 MB (51.98%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -1,53 +1,30 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Node root = new Node();
-        for (String word : wordDict)
-            addWord(word, root);
-        return wordBreak(s, 0, new HashMap<>(), root);
+        List<String> res = new ArrayList<>();
+        backtrack(res,s,wordDict, new ArrayList<>(), 0);
+        return res;
     }
-    
-    public void addWord(String word, Node root) {
-        for(int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (!root.map.containsKey(c))
-                root.map.put(c, new Node());
-            root = root.map.get(c);
+    private void backtrack(List<String> res,String s, List<String> wordDict, List<String> current, int idx){
+        if(idx == s.length()){
+            res.add(buildString(current));
         }
-        root.end = true;
-    }
-    
-    public List<String> wordBreak(String s, int index, Map<Integer, List<String>> dp, Node root) {
-        if (index == s.length()) {
-            return Collections.singletonList("");
-        }
-        
-        if (dp.containsKey(index)) {
-            return dp.get(index);
-        }
-        
-        Node curr = root;
-        StringBuilder sb = new StringBuilder("");
-        for(int i = index; i < s.length(); i++) {
-            char c = s.charAt(i);
-            sb.append(c);
-            if (!curr.map.containsKey(c))
-                break;
-            if (curr.map.get(c).end) {
-                List<String> out = wordBreak(s, i + 1, dp, root);
-                List<String> result = dp.getOrDefault(index, new ArrayList<>());
-                String sub = sb.toString();
-                for (String o : out)
-                        result.add(sub + (o.isEmpty() ? "" : " ") + o);
-                dp.put(index, result);
+        for(String word : wordDict){
+            int len = word.length();
+            if(idx + len > s.length()) continue;
+            String substring = s.substring(idx, idx + len);
+            if(word.equals(substring)){
+                current.add(word);
+                backtrack(res,s,wordDict, current, idx + len);
+                current.remove(current.size() - 1);
             }
-            curr = curr.map.get(c);
         }
-        return dp.getOrDefault(index, new ArrayList<>());
     }
-    
-    
-    public static class Node {
-        Map<Character, Node> map = new HashMap<>();
-        boolean end = false;
+    private String buildString(List<String> list){
+        StringBuilder sb = new StringBuilder();
+        for(String s : list){
+            sb.append(s).append(" ");
+        }
+        if(sb.length() > 0) sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
